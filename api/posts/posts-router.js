60948ade2eeb5fe42e7c84fe1f7c5c409360b723
Cori Wooley <wooleycori@gmail.com>
2022-06-07 01:52:48 -0800

    created server, postsRouter, and get/getById request

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,53 @@
 // implement your posts router here
+const express = require('express')
+const router = express.Router()
+const Posts = require('./posts-model')
+
+router.get('/', (req, res) => {
+    Posts.find()
+    .then(posts => {
+        res.json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved"
+        })
+    })
+})
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const post = await Posts.findById(id)
+        if(!post){
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(post)
+        }
+    } catch{
+        res.status(500).json({
+            message: "The post information could not be retrieved"
+        })
+    }
+})
+
+router.post('/', (req, res) => {
+    //stuff
+})
+
+router.put('/:id', (req, res) => {
+    //stuff
+})
+
+router.delete('/:id', (req, res) => {
+    //stuff
+})
+
+router.get('/:id/comments', (req, res) => {
+    //stuff
+})
+
+
+module.exports = router
